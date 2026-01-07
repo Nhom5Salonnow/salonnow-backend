f55c9ac6e5f5d42d4621eddda26e3064118bca3c
hieuvovan0103 <govovanhieu@gmail.com>
2026-01-07 18:21:12 +0700

    fix main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
 
   const config = new DocumentBuilder()
     .setTitle('Swagger API')
