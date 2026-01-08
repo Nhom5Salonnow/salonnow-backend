0f17ad966debc08587a37be5c7352fc0faa12cc5
hieuvovan0103 <govovanhieu@gmail.com>
2026-01-08 10:31:33 +0700

    hot fix user-create.dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,7 +1,39 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+  IsPhoneNumber,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'khachhang@gmail.com',
+    description: 'Email đăng nhập',
+  })
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @ApiProperty({ example: '123456', description: 'Mật khẩu' })
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6, { message: 'Mật khẩu phải dài hơn 6 ký tự' })
   password: string;
+
+  @ApiProperty({ example: 'Nguyễn', description: 'Họ' })
+  @IsNotEmpty()
+  @IsString()
   firstName: string;
+
+  @ApiProperty({ example: 'Văn A', description: 'Tên' })
+  @IsNotEmpty()
+  @IsString()
   lastName: string;
+
+  @ApiProperty({ example: '0988888888', description: 'Số điện thoại' })
+  @IsNotEmpty()
+  @IsString()
   phoneNumber: string;
 }
