ed5787e89eab25e73ff193c319cf503aa47e868a
hieuvovan0103 <govovanhieu@gmail.com>
2026-01-08 12:14:52 +0700

    hot fix auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -13,14 +13,18 @@ export class AuthService {
   async validateUser(email: string, pass: string): Promise<any> {
     const user = await this.usersService.findByEmail(email);
 
-
-    if (user && (await bcrypt.compare(pass, user.password))) {
+    if (user && user.password && (await bcrypt.compare(pass, user.password))) {
       const { password, ...result } = user;
       return result;
     }
+
+    if (!user) console.log('Login fail: Email not found');
+    else if (!user.password)
+      console.log('Login fail: User has no password in DB');
+    else console.log('Login fail: Wrong password');
+
     return null;
   }
-
 
   async login(user: any) {
     const payload = { email: user.email, sub: user.id, role: user.role };
